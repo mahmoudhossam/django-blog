f1d7afc979b3106b6909cef52c246369613cb44b
Mahmoud Hossam <mahmoud.h.hanafy@gmail.com>
2012-09-28 06:54:51 +0200

    Add forms module.

---
+++ webapp/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from models import Post
+
+class SignupForm(forms.Form):
+    username = forms.CharField(max_length=20)
+    password = forms.CharField(widget=forms.PasswordInput, max_length=40)
+    email = forms.EmailField()
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
