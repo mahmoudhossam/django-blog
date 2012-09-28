1627f08054118185d4b8d5f90c2ab016c41e365f
Mahmoud Hossam <mahmoud.h.hanafy@gmail.com>
2012-09-28 06:53:29 +0200

    Rearrange post fields.

--- webapp/models.py
+++ webapp/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 
-# Create your models here.
-
 class Post(models.Model):
-    text = models.TextField(blank=False)
     title = models.CharField(blank=False, max_length=100)
+    text = models.TextField(blank=False)
     created_at = models.DateField(blank=False)
-
