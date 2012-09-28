cc55748e8d76ec59001e66797ca66f0ddf4f9b8f
Mahmoud Hossam <mahmoud.h.hanafy@gmail.com>
2012-09-28 06:51:39 +0200

    New post URL and view function.

--- blag/urls.py
+++ blag/urls.py
@@ -1,13 +1,12 @@
 from django.conf.urls import patterns, include, url
+from webapp import views
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'blag.views.home', name='home'),
-    # url(r'^blag/', include('blag.foo.urls')),
+    url(r'^new$', views.new_post),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
--- webapp/views.py
+++ webapp/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from django.template.response import TemplateResponse
+from models import Post
+from forms import *
+from util import *
+
+def new_post(request):
+    if request.method == 'GET':
+        return TemplateResponse(request, 'new.html', context={'form':PostForm()})
+    elif request.method == 'POST':
+        post_form = PostForm(request.POST)
+        if post_form.is_valid():
+            post_form.save()
+            return redirect('/')
