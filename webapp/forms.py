39c4b3399d7bc02b199462e14fe744b397995e95
Mahmoud Hossam <mahmoud.h.hanafy@gmail.com>
2012-09-30 09:31:44 +0200

    All signup fields are required.

--- webapp/forms.py
+++ webapp/forms.py
@@ -2,9 +2,9 @@ from django import forms
 from models import Post
 
 class SignupForm(forms.Form):
-    username = forms.CharField(max_length=20)
-    password = forms.CharField(widget=forms.PasswordInput, max_length=40)
-    email = forms.EmailField()
+    username = forms.CharField(max_length=20, required=True)
+    password = forms.CharField(widget=forms.PasswordInput, max_length=40, required=True)
+    email = forms.EmailField(required=True)
 
 class PostForm(forms.ModelForm):
 
