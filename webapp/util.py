acc2119d1158b0d9c40003a72687f6609646c234
Mahmoud Hossam <mahmoud.h.hanafy@gmail.com>
2012-10-06 11:57:50 +0200

    Add slug to posts, get a single post.

--- webapp/util.py
+++ webapp/util.py
@@ -1,9 +1,17 @@
 from models import *
 from datetime import date
-from django.shortcuts import redirect
+from django.shortcuts import redirect, get_object_or_404
+from django.template.defaultfilters import slugify
 
-def new_post(title, text):
+def create_post(title, text):
     post_date = date.today()
-    post = Post(title=title, text=text, created_at=post_date)
+    slug = slugify(title)
+    post = Post(title=title, text=text, created_at=post_date, slug=slug)
     post.save()
     return redirect('/')
+
+def get_post(year, month, day, slug):
+    year, month, day = map(int, (year, month, day))
+    post_date = date(year, month, day)
+    post = get_object_or_404(Post, created_at=post_date)
+    return post
